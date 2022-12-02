3193994475e3a5968ce22fa9ae55ea0ec90b5a43
EBarrett233 <ekbarrett211@stevenscollege.edu>
2022-12-02 10:10:21 -0500

    Update3

--- family_members_home.php
+++ family_members_home.php
@@ -1,19 +1,14 @@
 <!DOCTYPE html>
 <form>
     <div class="container">
-    <h1>Doctor Appointment</h1>
-    <label for="PatientID"><b>Patient ID:</b></label>
-    <input type="text" placeholder="ID" name="PatientID">
+    <h1>Family Member's Home</h1>
+    
     <label for="AppointDate"><b>Date</b></label>
     <input type="date" placeholder="Date" name="AppointDate">
-    <label for="PatientName"><b>Patient Name:</b></label>
-    <input type="text" placeholder="Name" name="PatientName">    
     <label for="DocDate"><b>Docter Name:</b></label>
     <input type="text" placeholder="Name" name="DocName">
-    <label for="DocAppointment"><b>Docter Appointment:</b></label>
-    <input type="text" placeholder="Appointment" name="DocAppointment">
     <label for="CareName"><b>Caregiver Name:</b></label>
-    <input type="text" placeholder="Name" name="CareName">
+    <input type="text" placeholder="Name" name="CareName">  
     <label for="MorningMed"><b>Morning Medicine:</b></label>
     <input type="text" placeholder="Med" name="MorningMed">
     <label for="AfternoonMed"><b>Afternoon Medicine:</b></label>
@@ -27,5 +22,11 @@
     <label for="Dinner"><b>Dinner:</b></label>
     <input type="text" placeholder="food" name="Dinner">
 
-    
-    </div>
+    <label for="family code"><b>Family Code(For Patient Family Member):</b></label>
+    <input type="text" placeholder="Family Code" name="family code">
+    <label for="PatientID"><b>Patient ID(For Patient Family Member):</b></label>
+    <input type="text" placeholder="ID" name="PatientID">
+
+
+    <button type="submit" class="continue"><strong>OK</strong></button>
+    <button type="submit" class="cancle"><strong>Cancle</strong></button>
