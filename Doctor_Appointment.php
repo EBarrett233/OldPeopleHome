4e0304508e325908fcc706ed82e0a47a92bcb604
EBarrett233 <ekbarrett211@stevenscollege.edu>
2022-12-06 10:08:14 -0500

    update7

--- Doctor_Appointment.php
+++ Doctor_Appointment.php
@@ -1,7 +1,19 @@
 <!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="Doctor_Appointment.css" />
+<head>
+    <div class="title">
+    <a href="Additional_Info_for_Patient.php">Patient Info</a>
+    <a href="Doctor_Appointment.php">Doctor Appointment</a>
+    <a href="role.php">Role</a>
+    <a href="Patient_Home.php">Patient Home</a>
+    <a href="Roster.php">Current Roster</a>
+    <a href="New_Roster">Make a new Roster</a>
+    <a href="payment.php">Make a Payment</a>
+</div>
+</head>
 <form>
-    <div class="container">
     <h1>Doctor Appointment</h1>
+    <div class="container">
     <label for="PatientID"><b>Patient ID:</b></label>
     <input type="text" placeholder="ID" name="PatientID">
     <label for="AppointDate"><b>Date</b></label>
@@ -12,10 +24,9 @@
     <option value="Doctor2">Doctor2</option>
     <option value="Doctor3">Doctor3</option>
     <option value="Doctor4">Doctor4</option>
+    </select>
     <label for="PatientName"><b>Patient Name:</b></label>
     <input type="text" placeholder="Name" name="PatientName">
+    </div>
     <button type="submit" class="continue"><strong>OK</strong></button>
     <button type="submit" class="cancle"><strong>Cancle</strong></button>
-
-    
-    </div>
