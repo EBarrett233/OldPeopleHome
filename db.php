af049c53fc688ad938f9e29cb1156e66d36b392a
Dane-Shaut <dshaut211@stevenscollege.edu>
2022-12-12 21:14:54 -0500

    test

--- db.php
+++ db.php
@@ -94,6 +94,9 @@
          VALUES('Admin','Dane','Shaut','admin@admin.com','7173810995','admin77','2003-05-05',' Approved')
          ON CONFLICT DO NOTHING;
 
+         INSERT INTO Log_info(Role,F_Name,L_Name,Email,Phone,Pwd,DOB,Approval)
+         VALUES('Supervisor','Zaccc','Deaa','sup@sup.com','7173810995','sup77','2003-05-05',' Approved')
+         ON CONFLICT DO NOTHING;
       "
 
       ;
